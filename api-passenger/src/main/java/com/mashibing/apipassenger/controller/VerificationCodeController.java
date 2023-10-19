49fa71c2633cb290a49a4b1e4cd4dd515e07cdbf
hanjp <2084490391@qq.com>
2023-10-19 23:59:32 +0800

    验证码校验骨架编写

--- api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/VerificationCodeController.java
@@ -5,6 +5,7 @@ import com.mashibing.apipassenger.service.VerificationCodeService;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,12 @@ public class VerificationCodeController {
         String passengerPhone = verificationCodeDTO.getPassengerPhone();
         System.out.println("接受的手机号是："+passengerPhone);
         return verificationCodeService.generatorCode(passengerPhone);
+    }
+    @PostMapping("/verification-code-check")
+    public ResponseResult checkVerificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+        String verificationCode = verificationCodeDTO.getVerificationCode();
+        System.out.println("手机号：" + passengerPhone + " 验证码：" + verificationCode);
+        return verificationCodeService.checkCode(passengerPhone,verificationCode);
     }
 }
--- api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/service/VerificationCodeService.java
@@ -2,6 +2,7 @@ package com.mashibing.apipassenger.service;
 
 import com.mashibing.apipassenger.remote.ServiceVerificationcodeClient;
 import com.mashibing.internalcommon.Response.NumberCodeResponse;
+import com.mashibing.internalcommon.Response.TokenResponse;
 import com.mashibing.internalcommon.dto.ResponseResult;
 import net.sf.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,12 @@ public class VerificationCodeService {
     private StringRedisTemplate stringRedisTemplate;
 
     private String verificationCodeprefix = "passenger-verification-code-";
+
+    /**
+     * 生成验证码
+     * @param passengerPhone
+     * @return
+     */
     public ResponseResult generatorCode(String passengerPhone){
         //调用验证码服务，获取验证码
         System.out.println("调用验证码服务，获取验证码");
@@ -31,4 +38,31 @@ public class VerificationCodeService {
         stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
         return ResponseResult.success("");
     }
+
+    /**
+     * 校验验证码
+     * @param passengerPhone
+     * @param verificationCode
+     * @return
+     */
+    public ResponseResult checkCode(String passengerPhone, String verificationCode){
+        //根据手机号，去redis读取验证码
+        System.out.println("根据手机号，去redis读取验证码");
+
+        //校验验证码
+        System.out.println("校验验证码");
+
+        //判断原来是有用户，并进行对应的处理
+        System.out.println("判断原来是有用户，并进行对应的处理");
+
+        //颁发令牌
+        System.out.println("颁发令牌");
+
+        //响应
+        TokenResponse tokenResponse = new TokenResponse();
+        tokenResponse.setToken("token value");
+        return ResponseResult.success(tokenResponse);
+    }
+
+
 }
