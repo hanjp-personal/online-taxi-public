572bc7d4b8ec15e4cfa96e522ccbd1eb4cf745ed
hanjp <2084490391@qq.com>
2023-10-20 23:09:12 +0800

    插入用户代码编写

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -6,6 +6,7 @@ import com.mashibing.servicepassengeruser.mapper.PassengerUserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,13 +17,24 @@ public class UserService {
     @Autowired
     private PassengerUserMapper passengerUserMapper;
     public ResponseResult loginOrRegister(String passengerPhone){
-        System.out.println("user服务被调用了， 手机号是：" + passengerPhone);
-
+        //根据手机号查询用户信息
         Map<String,Object> map = new HashMap<>();
         map.put("passenger_phone",passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers.size() == 0 ? "无记录" : passengerUsers.get(0).getPassengerName());
+        //判断用户信息是否存在
+        if(passengerUsers.size() == 0){
+            //用户信息不存在
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setState((byte) 0);
 
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+            passengerUserMapper.insert(passengerUser);
+        }
 
         return ResponseResult.success();
     }
