64944c64c9dc3ff6b071805ddaf341e13eb74494
hanjp <2084490391@qq.com>
2023-10-22 20:49:17 +0800

    token测试代码编写

--- api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mashibing.apipassenger.controller;
 
+import com.mashibing.internalcommon.dto.ResponseResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,23 @@ public class TestController {
     @GetMapping("/test")
     public String test(){
         return "test api passenger";
+    }
+
+    /**
+     * 需要token
+     * @return
+     */
+    @GetMapping("/authtest")
+    public ResponseResult authtest(){
+        return ResponseResult.success("auth test");
+    }
+
+    /**
+     * 不需要token
+     * @return
+     */
+    @GetMapping("/noauthtest")
+    public ResponseResult noauthtest(){
+        return ResponseResult.success("noauth test");
     }
 }
