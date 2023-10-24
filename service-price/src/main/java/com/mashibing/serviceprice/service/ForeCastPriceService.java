62e2208382c1236922681421fdaf8fa83ce7d24f
hanjp <2084490391@qq.com>
2023-10-24 17:21:44 +0800

    service-price服务调用service-map服务

--- service-price/src/main/java/com/mashibing/serviceprice/service/ForeCastPriceService.java
+++ service-price/src/main/java/com/mashibing/serviceprice/service/ForeCastPriceService.java
@@ -1,13 +1,20 @@
 package com.mashibing.serviceprice.service;
 
+import com.mashibing.internalcommon.Response.DirectionResponse;
 import com.mashibing.internalcommon.Response.ForeCastPriceResponse;
 import com.mashibing.internalcommon.dto.ResponseResult;
+import com.mashibing.internalcommon.request.ForeCastPriceDTO;
+import com.mashibing.serviceprice.remote.ServiceMapClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class ForeCastPriceService {
+
+    @Autowired
+    private ServiceMapClient serviceMapClient;
 
     public ResponseResult forecasrPrice(String depLongitude, String deplatitude,String destLongitude,String destlatitude){
         log.info("出发地经度： "+ depLongitude);
@@ -16,6 +23,15 @@ public class ForeCastPriceService {
         log.info("目的地纬度： "+ destlatitude);
 
         log.info("调用地图服务，查询距离和时长");
+        ForeCastPriceDTO foreCastPriceDTO = new ForeCastPriceDTO();
+        foreCastPriceDTO.setDepLongitude(depLongitude);
+        foreCastPriceDTO.setDeplatitude(deplatitude);
+        foreCastPriceDTO.setDestLongitude(destLongitude);
+        foreCastPriceDTO.setDestlatitude(destlatitude);
+        ResponseResult<DirectionResponse> direction = serviceMapClient.direction(foreCastPriceDTO);
+        Integer distance = direction.getData().getDistance();
+        Integer duration = direction.getData().getDuration();
+        log.info(("距离："+distance + "时长： "+duration));
 
         log.info("读取计价规则");
 
