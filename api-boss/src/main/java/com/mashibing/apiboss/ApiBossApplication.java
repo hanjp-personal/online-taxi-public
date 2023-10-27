e8e55a1619c0a9f91668b4ae26acf384b9bd3ba5
hanjp <2084490391@qq.com>
2023-10-27 14:57:31 +0800

    api-boss项目创建和注册到nacos服务中

---
+++ api-boss/src/main/java/com/mashibing/apiboss/ApiBossApplication.java
@@ -0,0 +1,11 @@
+package com.mashibing.apiboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBossApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiBossApplication.class);
+    }
+}
