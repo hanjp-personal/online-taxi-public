f9cd85a05c054507059cd5fcad7b80b58f828567
hanjp <2084490391@qq.com>
2023-10-28 22:47:32 +0800

    自定义生成car表对应的代码

---
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/controller/CarController.java
@@ -0,0 +1,20 @@
+package com.mashibing.servicedriveruser.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author hanjp
+ * @since 2023-10-28
+ */
+@Controller
+@RequestMapping("/car")
+public class CarController {
+
+}
---
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/generator/MysqlGenerator.java
@@ -0,0 +1,26 @@
+package com.mashibing.servicedriveruser.generator;
+
+import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
+
+/**
+ * 自动生成代码类
+ */
+public class MysqlGenerator {
+
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/service-driver-user?characterEncoding=utf-8&serverTimezone=GMT%2B8","root","root@123")
+                .globalConfig(builder -> {
+                    builder.author("hanjp").fileOverride().outputDir("/Users/hanjianpeng/Idea_Workspace/online-taxi-public/service-driver-user/src/main/java");
+                }).packageConfig(builder -> {
+                    builder.parent("com.mashibing.servicedriveruser").pathInfo(Collections.singletonMap(OutputFile.mapperXml,
+                            "/Users/hanjianpeng/Idea_Workspace/online-taxi-public/service-driver-user/src/main/java/com/mashibing/servicedriveruser/mapper"));
+                }).strategyConfig(builder -> {
+                    builder.addInclude("car");
+                }).templateEngine(new FreemarkerTemplateEngine())
+                .execute();
+    }
+}
