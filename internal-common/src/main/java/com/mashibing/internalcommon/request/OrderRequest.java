e10bd80702da9e98dfb2aea44cc4692603e9e3f2
hanjp <2084490391@qq.com>
2023-11-02 11:06:33 +0800

    api-passenger 请求参数获取

---
+++ internal-common/src/main/java/com/mashibing/internalcommon/request/OrderRequest.java
@@ -0,0 +1,32 @@
+package com.mashibing.internalcommon.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+public class OrderRequest {
+
+    private String address;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime departtime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime ordertime;
+
+    private String departure;
+
+    private String deplongitude;
+
+    private String deplatitude;
+
+    private String destination;
+
+    private String deslongitude;
+
+    private String deslatitude;
+
+    private Integer encrypt;
+
+    private String faretype;
+
+}
