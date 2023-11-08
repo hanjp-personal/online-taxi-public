09ae2e9f46e88b31d52f9c6f10c4e821403f410f
hanjp <2084490391@qq.com>
2023-11-08 12:23:09 +0800

    redisson分布式锁配置代码优化

--- service-order/src/main/java/com/mashibing/serviceorder/config/RedisConfig.java
+++ service-order/src/main/java/com/mashibing/serviceorder/config/RedisConfig.java
@@ -3,11 +3,19 @@ package com.mashibing.serviceorder.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConfig {
+
+    private String potocol = "redis://";
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private String port;
 
     /**
      * 配置Redisson分布式锁
@@ -16,7 +24,7 @@ public class RedisConfig {
     @Bean
     public RedissonClient redissonClient(){
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+        config.useSingleServer().setAddress(potocol+host+":"+port).setDatabase(0);
         return Redisson.create(config);
     }
 }
