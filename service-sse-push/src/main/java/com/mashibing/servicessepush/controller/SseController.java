8b2e21a9916ecee758afcfc05ee28846edca475e
hanjp <2084490391@qq.com>
2023-11-08 17:05:22 +0800

    消息服务改造完成

--- service-sse-push/src/main/java/com/mashibing/servicessepush/controller/SseController.java
+++ service-sse-push/src/main/java/com/mashibing/servicessepush/controller/SseController.java
@@ -1,7 +1,8 @@
 package com.mashibing.servicessepush.controller;
 
+import com.mashibing.internalcommon.util.SsePrefixUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -11,35 +12,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class SseController {
 
     public static Map<String,SseEmitter> stringSseEmitterMap = new HashMap<>();
 
     /**
      * 建立连接
-     * @param driverId
+     * @param userId 用户Id
+     * @param identity 身份类型
      * @return
      */
-    @GetMapping("/connect/{driverId}")
-    public SseEmitter connect(@PathVariable String driverId){
-        System.out.println("司机ID: "+ driverId);
+    @GetMapping("/connect")
+    public SseEmitter connect(@RequestParam Long userId,@RequestParam String identity){
+        log.info("用户："+userId+"身份类型："+identity);
         SseEmitter sseEmitter = new SseEmitter(0l);
-
-        stringSseEmitterMap.put(driverId,sseEmitter);
+        String sseMapKey = SsePrefixUtils.generatorSseKey(userId,identity);
+        stringSseEmitterMap.put(sseMapKey,sseEmitter);
         return sseEmitter;
     }
 
     /**
      * 推送消息
-     * @param driverId 消息推送者
-     * @param content 消息内容
+     * @param userId 用户Id
+     * @param identity 身份类型
+     * @param content 内容
      * @return
      */
     @GetMapping("/push")
-    public String push(@RequestParam String driverId, @RequestParam String content){
-        SseEmitter sseEmitter = stringSseEmitterMap.get(driverId);
+    public String push(@RequestParam Long userId, @RequestParam String identity, @RequestParam String content){
+        String sseMapKey = SsePrefixUtils.generatorSseKey(userId,identity);
+        SseEmitter sseEmitter = stringSseEmitterMap.get(sseMapKey);
         try {
-            if (stringSseEmitterMap.containsKey(driverId)){
+            if (stringSseEmitterMap.containsKey(sseMapKey)){
                 sseEmitter.send(content);
             }else {
                 return "推送失败";
@@ -47,14 +52,15 @@ public class SseController {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return "给用户"+driverId+", 发送了消息内容："+content;
+        return "给用户"+sseMapKey+", 发送了消息内容："+content;
     }
 
-    @GetMapping("/close/{driverId}")
-    public String close(@PathVariable String driverId){
-        System.out.println("关闭连接："+driverId);
-        if (stringSseEmitterMap.containsKey(driverId)){
-            stringSseEmitterMap.remove(driverId);
+    @GetMapping("/close")
+    public String close(@RequestParam Long userId,@RequestParam String identity){
+        String sseMapKey = SsePrefixUtils.generatorSseKey(userId,identity);
+        log.info("关闭连接："+sseMapKey);
+        if (stringSseEmitterMap.containsKey(sseMapKey)){
+            stringSseEmitterMap.remove(sseMapKey);
         }
         return "close success";
     }
