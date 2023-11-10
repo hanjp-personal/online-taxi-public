b69aab2b4c396651f01f6127603dbd0cdf5bf448
hanjp <2084490391@qq.com>
2023-11-10 16:01:31 +0800

    支付宝沙箱回调业务成功

--- test-alipay/src/main/java/com/mashibing/testalipay/AliPayController.java
+++ test-alipay/src/main/java/com/mashibing/testalipay/AliPayController.java
@@ -4,8 +4,13 @@ import com.alipay.easysdk.factory.Factory;
 import com.alipay.easysdk.payment.page.models.AlipayTradePagePayResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @ResponseBody
@@ -21,6 +26,33 @@ public class AliPayController {
             throw new RuntimeException(e);
         }
         return response.getBody();
+
+    }
+
+    @PostMapping("/notify")
+    public String notify(HttpServletRequest request) throws Exception {
+        String tradeStatus = request.getParameter("trade_status");
+
+        if (tradeStatus.trim().equals("TRADE_SUCCESS")){
+            Map<String,String> param = new HashMap<>();
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            for (String name: parameterMap.keySet()) {
+                param.put(name,request.getParameter(name));
+            }
+            if (Factory.Payment.Common().verifyNotify(param)){
+                System.out.println("支付宝验证通过");
+                for (String nameParam : param.keySet()) {
+                    System.out.println("收到并且接受的参数：");
+                    System.out.println(nameParam + "，"+ param.get(nameParam));
+                }
+            }else {
+                System.out.println("支付宝验证不通过");
+            }
+
+
+        }
+        return "success";
+
 
     }
 }
